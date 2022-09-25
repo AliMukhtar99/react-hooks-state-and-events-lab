0e330e0f7f257d4984eab507316cf857583c7d04
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-09-25 13:55:34 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [items, setItems] = useState(itemData);
+  const [isDark, setDark] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // this data will be passed down to the ShoppingList as a prop
+  console.log(items);
+
+  function handleDarkClick() {
+    setDark((darkMode) => !darkMode);
+  }
 
   return (
-    <div className={appClass}>
+    <div className={"App " + (isDark ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkClick}>
+          {isDark ? "Dark Mode" : "Light Mode"}
+        </button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCategory] = useState("All");
+
+  function handleSelect(event) {
+    console.log(event.target.value);
+    setCategory(event.target.value);
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleSelect}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +20,13 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.map((item) => {
+          console.log(selectedCategory);
+          if (selectedCategory === "All" || item.category === selectedCategory)
+            return (
+              <Item key={item.id} name={item.name} category={item.category} />
+            );
+        })}
       </ul>
     </div>
   );
